cb6b336d417f3b6d3ec26f0a3fa0eec334a2d0c6
George Philippas <george.philippas@outlook.com.gr>
2026-09-19 16:08:18 +0300

    COMMIT

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ from src.launcher import launch_console
 from src.openai_integration import openai_construct_exercise
 
 if __name__ == "__main__":
-    print(launch_console(openai_construct_exercise(2)))
+    print(launch_console(openai_construct_exercise(10)))
