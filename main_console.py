ae16af1288ac640e8e466c64ae1838bf20e1eb23
George Philippas <george.philippas@outlook.com.gr>
2026-09-20 23:24:26 +0300

    COMMIT

--- main_console.py
+++ main_console.py
@@ -1,5 +1,6 @@
+from src.domain import Vocabulary
 from src.launcher import launch_console
 from src.openai_integration import openai_construct_exercise
 
 if __name__ == "__main__":
-    print(launch_console(openai_construct_exercise(questions_number=3)) * 100)
+    print(launch_console(openai_construct_exercise(questions_number=3, vocabulary_=Vocabulary.FRENCH)) * 100)
