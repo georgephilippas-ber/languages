30c01f49bca928183321c515a27f3fc9714643f1
George Philippas <george.philippas@outlook.com.gr>
2026-09-21 02:47:01 +0300

    COMMIT

--- main_console.py
+++ main_console.py
@@ -1,6 +1,6 @@
-from src.domain import Vocabulary
+from src.domain import Vocabulary, CEFRLevel
 from src.launcher import launch_console
 from src.openai_integration import openai_construct_exercise
 
 if __name__ == "__main__":
-    print(launch_console(openai_construct_exercise(questions_number=10, vocabulary_=Vocabulary.GERMAN)) * 100)
+    print(launch_console(openai_construct_exercise(questions_number=2, vocabulary_=Vocabulary.GERMAN, cefr_level_=CEFRLevel.A2)) * 100)
