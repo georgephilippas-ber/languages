ac9fce71d4cbb551d4e6e20cbb1d2dfb019ab08f
George Philippas <george.philippas@outlook.com.gr>
2026-09-20 15:41:06 +0300

    COMMIT

---
+++ src/database.py
@@ -0,0 +1,20 @@
+from os.path import sep
+from sqlite3 import connect, Connection
+
+from typing import List
+
+DATABASE_PATH_ELEMENTS: List[str] = ["..", "vocabulary", "history"]
+DATABASE_NAME: str = "history.db"
+
+
+def get_database_connection() -> Connection:
+    return connect(sep.join(DATABASE_PATH_ELEMENTS + [DATABASE_NAME]))
+
+
+def close_connection(connection: Connection):
+    connection.close()
+
+
+if __name__ == "__main__":
+    connection_ = get_database_connection()
+    close_connection(connection_)
