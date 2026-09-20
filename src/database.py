ad7bc60d4554a4748f3a668d50d1d4f6641e3a7b
George Philippas <george.philippas@outlook.com.gr>
2026-09-20 15:50:17 +0300

    COMMIT

--- src/database.py
+++ src/database.py
@@ -15,6 +15,20 @@ def close_connection(connection: Connection):
     connection.close()
 
 
+def create_table(connection: Connection):
+    connection.execute(
+        """
+        CREATE TABLE IF NOT EXISTS vocabulary
+        (
+            id              INTEGER PRIMARY KEY,
+            term            TEXT NOT NULL,
+            language        TEXT NOT NULL,
+            last_trained_at TEXT
+        )
+        """
+    )
+
+
 if __name__ == "__main__":
     connection_ = get_database_connection()
     close_connection(connection_)
